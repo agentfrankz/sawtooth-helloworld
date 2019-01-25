69a7fd62fb968bf5916f68b1bc212fcf6de32649
agentfrankz <franklin.john@iiitmk.ac.in>
2019-01-25 13:59:58 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-Steps to run the project
-
-1. git clone https://gitlab.com/agentfrankz/sawtooth-helloworld.git
+1. clone the repo
 2. cd sawtooth-helloworld
-3. npm install
-4. cd helloworldprocessor
-5. npm install
-6. cd ..
-7. npm start
-8. In new terminal type - sudo docker-compose up
+3. sudo docker-compose up
+4. visit page localhost:3000
--- helloworldprocessor/HelloWorldProcessor.js
+++
@@ -1,8 +0,0 @@
-const { TransactionProcessor } = require('sawtooth-sdk/processor');
-const HelloWorldHandler = require('./HelloWorldHandler');
-
-const address = 'tcp://validator:4004';
-console.log('Connected---------------------------------------------------------------------');
-const transactionProcesssor = new TransactionProcessor(address);
-transactionProcesssor.addHandler(new HelloWorldHandler());
-transactionProcesssor.start();
